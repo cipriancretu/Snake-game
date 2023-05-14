b571aceaf14fc65135fba2104a927f1c788b40d8
Ciprian Cretu <124049021+cipriancretu@users.noreply.github.com>
2023-05-14 18:23:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
-"""Import modules and classes"""
+"""Libraries"""
+import time
 from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
-import time
+
 
 """set up the screen"""
 screen = Screen()
@@ -30,18 +31,18 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with wall.
+    # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
 
-    #Detect collision with tail.
+    # Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
